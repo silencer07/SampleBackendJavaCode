bbde14bcadc60abdc71d6dc57341f42e004ee47f
aldrin tingson <aldrin tingson>
2016-09-08 18:09:15 +0800

    added dependency and fongo config

--- src/main/java/com/englishcentral/AldrinTingsonApplication.java
+++ src/main/java/com/englishcentral/AldrinTingsonApplication.java
@@ -1,12 +1,31 @@
 package com.englishcentral;
 
+import com.github.fakemongo.Fongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 public class AldrinTingsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AldrinTingsonApplication.class, args);
+	}
+
+	@Bean
+	public Fongo fongo(){
+		return new Fongo("InMemoryMongo");
+	}
+
+	@Bean
+	public MongoClient mongo(Fongo fongo){
+		return fongo.getMongo();
 	}
 }
