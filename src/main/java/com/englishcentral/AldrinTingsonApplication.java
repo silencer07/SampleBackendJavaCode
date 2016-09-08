cc9679bc9386c4ffa14a46b73f0e5fda92c2296f
aldrin tingson <aldrin tingson>
2016-09-08 11:14:55 +0800

    initial commit

---
+++ src/main/java/com/englishcentral/AldrinTingsonApplication.java
@@ -0,0 +1,12 @@
+package com.englishcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldrinTingsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldrinTingsonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/englishcentral/AldrinTingsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.englishcentral;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AldrinTingsonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
