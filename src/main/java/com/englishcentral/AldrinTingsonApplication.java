39d798c4e67c562a553a638bd215fe136729bcc9
aldrin tingson <aldrin tingson>
2016-09-09 09:32:07 +0800

    added sample data

--- src/main/java/com/englishcentral/AldrinTingsonApplication.java
+++ src/main/java/com/englishcentral/AldrinTingsonApplication.java
@@ -1,10 +1,13 @@
 package com.englishcentral;
 
+import com.englishcentral.video.Video;
+import com.englishcentral.video.VideoRepository;
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,8 @@ import org.springframework.context.annotation.Configuration;
 public class AldrinTingsonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AldrinTingsonApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AldrinTingsonApplication.class, args);
+		initializeSampleData(context.getBean(VideoRepository.class));
 	}
 
 	@Bean
@@ -27,5 +31,21 @@ public class AldrinTingsonApplication {
 	@Bean
 	public MongoClient mongo(Fongo fongo){
 		return fongo.getMongo();
+	}
+
+	public static void initializeSampleData(VideoRepository videoRepository){
+		Video video = new Video();
+		video.setName("Sample Data 1");
+		video.setDescription("Sample Data description 1");
+		video.setLengthInSecs(180);
+		video.setUploadedBy("SampleUploader1");
+		videoRepository.save(video);
+
+		Video video2 = new Video();
+		video2.setName("Sample Data 2");
+		video2.setDescription("Sample Data description 2");
+		video2.setLengthInSecs(240);
+		video2.setUploadedBy("SampleUploader2");
+		videoRepository.save(video2);
 	}
 }
